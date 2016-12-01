3ca7d3f200f870b5671a82ebbcccfd48c9fc0f78
gnani-varkala <varkala.gnani@gmail.com>
2016-12-01 18:19:35 +0530

    $http using service

--- service_directive/JS/controller.js
+++ service_directive/JS/controller.js
@@ -1,31 +1,47 @@
-var app = angular.module("angular_app",[]);
+(function(){
+  'use strict';
+  angular.module('angular_app',[]);
+})();
 
-app.controller('jsonCtrl',['$scope','jsonService',function($scope,jsonService){
-  $scope.jsonDetails =[];
-  $scope.callBack = function(data){
-      $scope.jsonDetails = data;
-  }
-  jsonService.myJson('JSON/id.json',$scope.callBack);
-  $scope.delete = function(data){
-      console.log(data);
-        $scope.jsonDetails.splice(data,1);
+(function () {
+    'use strict';
 
-  }
+    angular
+        .module('angular_app')
+        .controller('jsonCtrl', jsonCtrl);
 
-}]);
+    jsonCtrl.$inject = ['$scope','jsonService'];
+    function jsonCtrl($scope,jsonService){
+      $scope.jsonDetails =[];
+      $scope.callBack = function(data){
+          $scope.jsonDetails = data;
+      }
+      jsonService.myJson('JSON/id.json',$scope.callBack);
+      $scope.delete = function(data){
+            $scope.jsonDetails.splice(data,1);
 
+      }
+    }
+})();
 
-app.controller('addressCtrl',['$scope','jsonService',function($scope,jsonService){
-  $scope.addressJSON=[];
-  $scope.callBack = function(data){
-      $scope.addressJSON = data;
-  }
-  jsonService.myJson('JSON/address.json',$scope.callBack);
+(function () {
+    'use strict';
 
-  $scope.delete = function(data){
-      console.log(data);
-        $scope.addressJSON.splice(data,1);
+    angular
+        .module('angular_app')
+        .controller('addressCtrl', addressCtrl);
 
-  }
+    addressCtrl.$inject = ['$scope','jsonService'];
+    function addressCtrl($scope,jsonService){
+      $scope.addressJSON =[];
+      $scope.callBack = function(data){
+          $scope.addressJSON = data;
+      }
+      jsonService.myJson('JSON/address.json',$scope.callBack);
+      $scope.delete = function(data){
+            $scope.addressJSON.splice(data,1);
 
-}]);
+      }
+    }
+
+})();
