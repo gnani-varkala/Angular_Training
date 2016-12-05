51d7aa796d8367fe0bd3e7995a442f174a9673d2
gnani-varkala <varkala.gnani@gmail.com>
2016-12-05 12:29:48 +0530

    Less and filters

--- less_filters/JS/controller.js
+++ less_filters/JS/controller.js
@@ -17,35 +17,37 @@
       jsonService.myJson('JSON/example.json').then(function(data){
         $scope.jsonDetails = data;
       });
-      $scope.select1 = {select:false};
-      $scope.select2 = {select:false};
-      $scope.select3 = {select:false};
-      $scope.select4 = {select:false};
 
-      $scope.selectedRow = null;  // initialize our variable to null
-      $scope.setClickedRow = function(index){  //function that sets the value of selectedRow to current index
-         $scope.selectedRow = index;
-      }
+      $scope.selectedRow = null;
+    $scope.setClickedRow = function(index){  
+       $scope.selectedRow = index;
+    }
 
       $scope.color1 = function(e){
         var x = event.target;
         console.log(x);
-        $scope.select1.select = !$scope.select1.select;
+        $scope.select1 = {select:true};
+          $scope.select2="";
+          $scope.select3="";
+          $scope.select4="";
       }
       $scope.color2 = function(){
-        var x = event.target;
-        console.log(x);
-        $scope.select2.select = !$scope.select2.select;
+        $scope.select2 = {select:true};
+        $scope.select1="";
+        $scope.select3="";
+        $scope.select4="";
       }
       $scope.color3 = function(){
-        var x = event.target;
-        console.log(x);
-        $scope.select3.select = !$scope.select3.select;
+        $scope.select3 = {select:true};
+        $scope.select1="";
+        $scope.select2="";
+        $scope.select4="";
       }
       $scope.color4 = function(){
-        var x = event.target;
-        console.log(x);
-        $scope.select4.select = !$scope.select4.select;
+        $scope.select4 = {select:true};
+        $scope.select1="";
+        $scope.select2="";
+        $scope.select3="";
       }
     }
 })();
